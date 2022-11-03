3c97fba44d2633f976266a3e320973eef34190a3
José Henrique Martins Pinheiro <pinheiro012345@gmail.com>
2022-11-03 14:59:51 -0300

    ferramenta de teste

--- teste_funcional.py
+++ teste_funcional.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-nav = webdriver.Chrome()
-nav.get('http://localhost:8000')
 
-assert 'Django' in nav.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.nav = webdriver.Chrome()
+
+    def tearDown(self):
+        self.nav.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith ouviu falar de uma nova aplicação interessante
+        # Lista de tarefas. Verificar a homepage
+        self.nav.get('http://localhost:8000')
+
+        # Ela percebe que o titulo da pagina e o cabecalho mencionam lista de tarefas (to-do)
+        self.assertIn('To-do', self.nav.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
